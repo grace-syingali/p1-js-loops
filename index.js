15cbf7c39032efcf267fbbe96cc1065cf13359d7
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2023-06-20 21:40:15 +0300

    Add simple for loop exercise

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1. Simple FOR loop
+// TODO: Complete the function. Create a For loop that loops through and prints numbers 1 to 10
+function loopThroughNumbers(){
+  // create a for loop with an initialization, stopping condition & an iteration statement
+
+}
+
+// loopThroughNumbers();
+
