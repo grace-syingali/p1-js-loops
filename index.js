707ceea54b365d200600650ccbca7a04fd8fea6c
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2023-06-20 22:28:03 +0300

    Add looping though arrays and object

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let books = ['Eloquent JavaScript', 'JavaScript: The Good Parts', 'Learn JavaScript VISUALLY', 'You don\'t know js', 'JavaScript: The Definitive Guide'];
+
+let oneBook = {
+  title: 'Eloquent JavaScript',
+  author: 'Marijin Haverbeke',
+  publisher: 'No Starch Press'
+}
+
 // 1. Simple FOR loop
 // TODO: Complete the function. Create a For loop that loops through and prints numbers 1 to 10
 function loopThroughNumbers(){
@@ -7,3 +15,35 @@ function loopThroughNumbers(){
 
 // loopThroughNumbers();
 
+// TODO: Complete the function. Loop and print through numbers 1 to 10 in reverse order
+function loopThroughNumbersReverse(){
+
+}
+
+// loopThroughNumbersReverse();
+
+// 2. Loop through arrays
+// TODO: Complete the function. Loop through the books array and print every element
+function loopThroughBooks(arr){
+  // create a for loop that loops through any array of books and logs each element to the console
+
+}
+
+// loopThroughBooks(books);
+
+// TODO: Complete the function. Loop through the books array and print every element using for...of
+function loopThroughBooks2(arr){
+
+}
+
+// loopThroughBooks2(books);
+
+// 3. Loop through objects
+// TODO: Complete the function. Loop through the properties of the oneBook object and log the values to the console
+function loopThroughObj(obj){
+  // Use for...in
+}
+
+
+
+// BONUS: Loop through the array of books using a while loop
